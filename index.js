9b542dfc0e00912f60b5796232921cf6bbfe31f8
atomic-zero <lkpanio25@gmail.com>
2025-02-17 07:40:19 +0800

    hahah

--- index.js
+++ index.js
@@ -1,70 +1,12 @@
 const { spawn } = require("child_process");
 const path = require("path");
-const { execSync } = require("child_process");
 
 const SCRIPT_FILE = "ddos.js";
 const SCRIPT_PATH = path.join(__dirname, SCRIPT_FILE);
 
-const npmPackages = ["axios@latest"];
-
 const restartEnabled = process.env.PID !== "0";
 
 let mainProcess;
-
-function isPackageUpToDate(pkg) {
-    const [packageName] = pkg.split("@");
-    try {
-        const localVersion = execSync(`npm list ${packageName} --depth=0 --json`, { cwd: __dirname, encoding: "utf8" });
-        const installedVersion = JSON.parse(localVersion).dependencies[packageName]?.version;
-
-        if (installedVersion) {
-            const latestVersion = execSync(`npm show ${packageName} version`, { encoding: "utf8" }).trim();
-            return installedVersion === latestVersion;
-        }
-    } catch (err) {
-        return false;
-    }
-    return false;
-}
-
-function installPackages(callback) {
-    console.log("Checking npm packages...");
-
-    let installCount = 0;
-    let totalPackages = npmPackages.length;
-
-    npmPackages.forEach((pkg) => {
-        if (isPackageUpToDate(pkg)) {
-            console.log(`Package ${pkg} is already up to date.`);
-            installCount++;
-            if (installCount === totalPackages) callback();
-            return;
-        }
-
-        console.log(`Installing package: ${pkg}...`);
-
-        const installProcess = spawn("npm", ["install", pkg], {
-            cwd: __dirname,
-            stdio: "inherit",
-            shell: true,
-        });
-
-        installProcess.on("error", (err) => {
-            console.error(`Error installing package ${pkg}:`, err);
-        });
-
-        installProcess.on("close", (exitCode) => {
-            installCount++;
-            if (exitCode !== 0) {
-                console.error(`Failed to install package ${pkg} with exit code [${exitCode}]`);
-            } else {
-                console.log(`Package ${pkg} installed successfully.`);
-            }
-
-            if (installCount === totalPackages) callback();
-        });
-    });
-}
 
 function start() {
     console.log("Starting main process...");
@@ -99,4 +41,4 @@ function restartProcess() {
     start();
 }
 
-installPackages(start);
+start();
